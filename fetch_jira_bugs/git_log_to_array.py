21d074f1bd7d802137324c15b8fd0ebd00a7df43
bakhtos <alexander.bakhtin@tuni.fi>
2021-09-24 13:13:26 +0300

    Save to user-defined path, fix keyword overloading

--- fetch_jira_bugs/git_log_to_array.py
+++ fetch_jira_bugs/git_log_to_array.py
@@ -8,15 +8,18 @@ import argparse
 import subprocess
 import sys
 import json
+import os
 
-def git_log_to_json(init_hash, path_to_repo):
+def git_log_to_json(init_hash, path_to_repo, path_to_save=None):
+    if path_to_save is None:
+        path_to_save = ''
     hashes = subprocess.run(['git', 'rev-list', init_hash], cwd=path_to_repo,
         stdout=subprocess.PIPE).stdout.decode('ascii').split()
 
     logs = []
     i = 0
-    for hash in hashes:
-        entry = subprocess.run(['git', 'show', '--quiet', '--date=iso', hash],
+    for hash_ in hashes:
+        entry = subprocess.run(['git', 'show', '--quiet', '--date=iso', hash_],
             cwd=path_to_repo, stdout=subprocess.PIPE)\
             .stdout.decode(errors='replace')
         logs.append(entry)
@@ -24,7 +27,12 @@ def git_log_to_json(init_hash, path_to_repo):
         if i % 10 == 0:
             print(i, end='\r')
 
-    with open('gitlog.json', 'w') as f:
+    try:
+        os.makedirs(path_to_save, exist_ok=True)
+    except FileNotFoundError:
+        pass
+    path = os.path.join(path_to_save, 'gitlog.json') 
+    with open(path, 'w') as f:
         f.write(json.dumps(logs))
 
 # Commits are saved in reverse chronological order from newest to oldest
@@ -36,9 +44,12 @@ if __name__ == '__main__':
             help="A SHA-1 representing a commit. Runs git rev-list from this commit.")
     parser.add_argument('--repo-path', type=str,
             help="The absolute path to a local copy of the git repository from where the git log is taken.")
+    parser.add_argument('--save-path', type=str,
+            help="Path to save 'gitlog.json' file."
 
     args = parser.parse_args()
     path_to_repo = args.repo_path
     init_hash = args.from_commit
-    git_log_to_json(init_hash, path_to_repo)
+    path_to_save = args.save_path
+    git_log_to_json(init_hash, path_to_repo, path_to_save)
 
